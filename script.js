558743ac519f6e601ef921ed5c2681b35372baa3
Cemile15 <qurbanlicemile344@gmail.com>
2024-01-07 00:23:46 +0400

    js-task1

---
+++ script.js
@@ -0,0 +1,51 @@
+
+//data1
+//1.Nihad ve Nail-in ceki ve boyunu deyishende saxla!
+
+let nihadKilo = 78;
+let nihadBoy = 169;
+let nailKilo = 92;
+let nailBoy = 195;
+
+
+//2.Onlarin BKI hesabla.
+
+birinci=nihadKilo/(nihadBoy**2);
+
+ikinci=nailKilo/(nailBoy**2);
+
+//3.Nihadin BHI-i Nailden boyukdurmu? Boolean-da saxla.
+
+if(birinci > ikinci){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+
+
+//data2
+////1.Nihad ve Nail-in cheki ve boyunu deyishende saxla!
+
+let nihad1Kilo = 95;
+let nihad1Boy = 188;
+let nail1Kilo = 85;
+let nail1Boy = 176;
+
+
+//2.Onlarin BKI hesabla.
+
+birinci1=nihad1Kilo/(nihad1Boy**2);
+
+ikinci1=nail1Kilo/(nail1Boy**2);
+
+
+//3.Nihadin BHI-i Nailden boyukdurmu? Boolean-da saxla.
+
+if(birinci1 > ikinci1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
